11d24fd788914968a904dd9a091025086b9c2852
Mahfuz Rahman <mahfuzrahman517@gmail.com>
2023-06-09 21:03:41 +0600

    [DONE] added code

--- src/app/middlewares/globalErrorHandeler.ts
+++ src/app/middlewares/globalErrorHandeler.ts
@@ -1,11 +1,10 @@
-/* eslint-disable no-unused-expressions */
-/* eslint-disable no-undef */
 import { ErrorRequestHandler, Request, Response, NextFunction } from 'express'
 import { IGenericErrorMessage } from '../../interfaces/error'
 import handleValidationError from '../../errors/handleValidationError'
 import config from '../../config'
 import ApiError from '../../errors/ApiError'
 import { errorlogger } from '../../shared/logger'
+import { ZodError } from 'zod'
 
 const globalErrorHandeler: ErrorRequestHandler = (
   error,
@@ -13,6 +12,7 @@ const globalErrorHandeler: ErrorRequestHandler = (
   res: Response,
   next: NextFunction
 ) => {
+  // eslint-disable-next-line no-unused-expressions
   config.env === 'development'
     ? console.log(`🐱‍🏍 globalErrorHandler ~~`, error)
     : errorlogger.error(`🐱‍🏍 globalErrorHandler ~~`, error)
@@ -20,6 +20,8 @@ const globalErrorHandeler: ErrorRequestHandler = (
   let statusCode = 500
   let message = 'Something went wrong !'
   let errorMessages: IGenericErrorMessage[] = []
+
+  console.log(error)
 
   if (error?.name === 'ValidationError') {
     const simplifiedError = handleValidationError(error)
@@ -37,23 +39,31 @@ const globalErrorHandeler: ErrorRequestHandler = (
           },
         ]
       : []
-  } else if (error instanceof Error) {
-    message = error?.message
-    errorMessages = error?.message
-      ? [
-          {
-            path: '',
-            message: error?.message,
-          },
-        ]
-      : []
+  } else if (error instanceof ZodError) {
+    // const simplifiedError = handleZodError(error)
+    // statusCode = simplifiedError?.statusCode
+    // message = simplifiedError.message
+    // errorMessages = simplifiedError.errorMessages
+    console.log(error)
   }
+  // else if (error instanceof Error) {
+  //   message = error?.message
+  //   errorMessages = error?.message
+  //     ? [
+  //         {
+  //           path: '',
+  //           message: error?.message,
+  //         },
+  //       ]
+  //     : []
+  // }
 
   res.status(statusCode).json({
     success: false,
     message,
     errorMessages,
     stack: config.env !== 'production' ? error?.stack : undefined,
+    // error,
   })
   next()
 }
